85b1a45c9068c58a240cd33fe2b3d31434bae99c
vip21-000 <serg091007@gmail.com>
2025-11-12 14:25:03 +0200

    qwerty123

--- greetings.py
+++ greetings.py
@@ -3,3 +3,6 @@ name = input("Enter your name: ")
 current_time = datetime.datetime.now().strftime("%%H:%%M:%%S") 
 print(f"Hello, {name}! Current time is {current_time}.") 
 print("buebuebue") 
+
+
+print("qwerty123") 
