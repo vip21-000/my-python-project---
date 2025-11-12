c8893859244e3687bf68a1c5ea18e273ab2873c3
Сергій Гриндула <serg091007@gmail.com>
2025-11-12 13:31:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Greetings Project 
+Simple Python project that greets the user and shows current time. 
---
+++ greetings.py
@@ -0,0 +1,4 @@
+import datetime 
+name = input("Enter your name: ") 
+current_time = datetime.datetime.now().strftime("%%H:%%M:%%S") 
+print(f"Hello, {name}! Current time is {current_time}.") 
